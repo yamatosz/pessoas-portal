6a8700886018d7631a8d927efcb0ecff05d6f551
Lucas Jose <90628627+yamatosz@users.noreply.github.com>
2023-03-28 22:03:14 -0300

    home page ui criada precisa terminar pagina de cadastro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
-
+import styles from './Home.module.css';
+import peoples from '../imgs/home.jpg'
+import LinkButton from '../layout/LinkButton';
 
 function Home() {
-    return (<div>
-        <h1>Home</h1>
-        <p>Bem vindo ao Portal Pessoas.</p>
-        <p>Bem-vindo ao nosso portal de cadastro de pessoas! Aqui, você pode criar um perfil online completo e personalizado com informações importantes sobre você, como foto, nome, idade, profissão e um breve resumo sobre si mesmo.
-            Com uma interface intuitiva e fácil de usar, nosso portal permite que você crie um perfil atraente que possa ser compartilhado com amigos, colegas de trabalho, familiares e outras pessoas importantes em sua vida. Com o perfil completo, você pode mostrar sua personalidade e habilidades, tornando-o mais atraente para oportunidades de trabalho, projetos e relacionamentos.</p>
-        <p>Não importa se você é um profissional experiente, um estudante universitário ou alguém que está apenas começando a construir sua carreira, nosso portal de cadastro de pessoas é o lugar perfeito para você se apresentar ao mundo e construir conexões importantes que podem ajudá-lo a atingir seus objetivos pessoais e profissionais.</p>
-    </div>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Portal Pessoas</span></h1>
+            <p>Faça seu cadastro agora mesmo!!</p>
+            <LinkButton to='/cadastro' text="Cadastre-se" />
+            <img src={peoples} alt="Pessoas" />
+
+        </section>
     )
 }
 
